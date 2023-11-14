f89199014c2661563ae7fe49c121c2091667c288
Kellyje <kejhirwa07@gmail.com>
2023-11-14 20:43:18 +0200

    initial files

---
+++ nestedif.dart
@@ -0,0 +1,17 @@
+void main (){
+  int age=45;
+  switch (age){
+    case 1:
+    print("Correct");
+    break;
+    case 3:
+    print ("wrong input");
+    break;
+
+    default:
+    print ("try again");
+    break;
+
+  }
+
+}
